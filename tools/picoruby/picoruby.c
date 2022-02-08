676d97d0040e87745343872f20a1929909fde6b6
HASUMI Hitoshi <hasumikin@gmail.com>
2022-02-08 17:13:29 +0900

    --verbose option

--- tools/picoruby/picoruby.c
+++ tools/picoruby/picoruby.c
@@ -29,23 +29,28 @@ void run(uint8_t *mrb)
   mrbc_vm_close(vm);
 }
 
+static bool verbose = false;
+
 int handle_opt(int argc, char * const *argv, char **oneliner)
 {
   struct option longopts[] = {
-    { "version",  no_argument,       NULL, 'v' },
-    { "oneline", required_argument,  NULL, 'e' },
-    { "loglevel", required_argument, NULL, 'l' },
-    { 0,          0,                 0,     0  }
+    { "version",   no_argument,       NULL, 'v' },
+    { "copyright", no_argument,       NULL, 'c' },
+    { "verbose",   no_argument,       NULL, 'V' },
+    { "help"   ,   no_argument,       NULL, 'h' },
+    { "evaluate",  required_argument, NULL, 'e' },
+    { "loglevel",  required_argument, NULL, 'l' },
+    { NULL,        0,                 NULL,  0  }
   };
   int opt;
   int longindex;
   loglevel = LOGLEVEL_ERROR;
-  while ((opt = getopt_long(argc, argv, "ve:l:", longopts, &longindex)) != -1) {
+  while ((opt = getopt_long(argc, argv, "vcVhe:l:", longopts, &longindex)) != -1) {
     switch (opt) {
       case 'v':
         DEBUGP("v add: %p\n", optarg);
         fprintf(stdout, "PicoRuby %s", PICORBC_VERSION);
-        #ifdef PICORBC_DEBUG
+        #ifdef PICORUBY_DEBUG
           fprintf(stdout, " (debug build)");
         #endif
         fprintf(stdout, "\n");
@@ -55,10 +60,6 @@ int handle_opt(int argc, char * const *argv, char **oneliner)
         break;
       case 'l':
         DEBUGP("l add: %p\n", optarg);
-        #ifndef PICORBC_DEBUG
-          fprintf(stderr, "[ERROR] `--loglevel=[level]` option is only valid if you made executable without -DNDEBUG\n");
-          return 1;
-        #endif
         if ( !strcmp(optarg, "debug") ) { loglevel = LOGLEVEL_DEBUG; } else
         if ( !strcmp(optarg, "info") )  { loglevel = LOGLEVEL_INFO; } else
         if ( !strcmp(optarg, "warn") )  { loglevel = LOGLEVEL_WARN; } else
@@ -69,8 +70,17 @@ int handle_opt(int argc, char * const *argv, char **oneliner)
           return 1;
         }
         break;
+      case 'c':
+        fprintf(stdout, "PicoRuby - Copyright (c) 2011-2022 HASUMI Hitoshi\n");
+        return -1;
+      case 'V':
+        verbose = true;
+        break;
+      case 'h':
+        fprintf(stdout, "Usage: (ToDo)\n");
+        return -1;
       default:
-        fprintf(stderr, "error! \'%c\' \'%c\'\n", opt, optopt);
+        fprintf(stderr, "piroruby: invalid option (-h will show valid options)\n");
         return 1;
     }
   }
@@ -85,6 +95,7 @@ int main(int argc, char *argv[])
 
   char *oneliner = NULL;
   int ret = handle_opt(argc, argv, &oneliner);
+  if (ret == -1) return 0;
   if (ret != 0) return ret;
 
   StreamInterface *si;
@@ -100,6 +111,7 @@ int main(int argc, char *argv[])
     if (si == NULL) return 1;
   }
   ParserState *p = Compiler_parseInitState(si->node_box_size);
+  p->verbose = verbose;
   if (Compiler_compile(p, si)) {
     run(p->scope->vm_code);
   } else {
@@ -108,7 +120,7 @@ int main(int argc, char *argv[])
 
   StreamInterface_free(si);
   Compiler_parserStateFree(p);
-#ifdef PICORBC_DEBUG
+#ifdef PICORUBY_DEBUG
   memcheck();
 #endif
   return ret;
